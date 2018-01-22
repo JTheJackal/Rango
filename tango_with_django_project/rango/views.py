0644d64b8e59dfec5bd59722f0660194689cad4c
Joshua Styles <styles@outlook.com>
2018-01-22 20:57:48 +0000

    Exercises
    Exercises complete

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,6 +1,11 @@
 from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse("Rango says 'hey there, partner'")
-
 # Create your views here.
+
+def index(request):
+    return HttpResponse("Rango says 'hey there, partner. Check out the <a href='/rango/about/'> about</a>  page.'")
+
+def about(request):
+    return HttpResponse("Rango says 'here is the about page. Go back <a href='/rango/'> here</a>'")
+
+
