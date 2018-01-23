2c01e16fd8f5175f7c535eb6177e712d46d4c85b
Joshua Styles <styles@outlook.com>
2018-01-23 02:17:13 +0000

    Static & Media Exercise
    Completed chapter 4 of the textbook and end of chapter exercise.

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,11 +1,22 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Rango says 'hey there, partner. Check out the <a href='/rango/about/'> about</a>  page.'")
+    #return HttpResponse("Rango says 'hey there, partner. Check out the <a href='/rango/about/'> about</a>  page.'")
+    #Construct a dictionary to pass to the template engine as its context
+    #Note the key boldmessage is the same as {{boldmessage}} in the template
+    context_dict = {'boldmessage': 'crunchy, creamy, cookie, candy, cupcake'}
+
+    #Return a rendered response to send to the client
+    #We make use of the shortcut function to make our lives easier
+    #Note that the first parameter is the template we wish to use
+    return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    return HttpResponse("Rango says 'here is the about page. Go back <a href='/rango/'> here</a>'")
+    return render(request, 'rango/about.html')
+
+    #return HttpResponse("Rango says 'here is the about page. Go back <a href='/rango/'>here</a>'")
 
 
